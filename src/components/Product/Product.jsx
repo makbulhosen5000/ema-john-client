bb99ee907c9c92d7151a7785d1c2b8dc0e154659
mh akash <mhakash5000@gmail.com>
2023-03-27 14:06:13 +0600

    project done

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { FaShoppingCart } from "react-icons/fa";
 const Product = (props) => {
+    
+    const handleAddToCart = props.handleAddToCart;
     const {img,name,price,quantity,seller,ratings} = props.product;
+   
     return (
         <div className="card w-full bg-base-100 shadow-2xl">
             <figure><img src={img} alt="Shoes" /></figure>
@@ -11,7 +14,7 @@ const Product = (props) => {
                 <h2 className="card-title">Rating : {ratings} </h2>
                 <h2 className="card-title">Manufacturer : {seller} </h2>
             </div>
-            <button className="btn btn-primary w-full">Buy Now <FaShoppingCart/></button>
+            <button onClick={handleAddToCart} className="btn btn-success w-full">Buy Now <span className=' ml-1 text-red-50'><FaShoppingCart/></span></button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
+
 const Shop = () => {
     const [products,setProducts] = useState([]);
+    const [cart,setCart] = useState([]);
+
     useEffect(()=>{
         const loadData = async()=>{
             const res = await fetch('products.json');
@@ -11,16 +14,27 @@ const Shop = () => {
         }
         loadData();
     },[]);
+   
+    //add to cart
+    const handleAddToCart = (product) =>{
+       const newCart = [...cart,product];
+       setCart(newCart);
+    }
+
     return (
            <div className='sm:flex-1 md:flex lg:flex'>
                  <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-10 px-20'>
                     {
-                    products.map((product)=> <Product product={product} key={product.key} />)
+                    products.map((product)=> <Product
+                    product={product}
+                    handleAddToCart={handleAddToCart}
+                    key={product.key} />)
                     }
                 </div>
-                <div className="w-96 sm:h-screen md:h-screen lg:h-screen  bg-warning shadow-xl">
+                <div className="w-full sm:h-screen md:h-screen lg:h-screen  bg-warning shadow-xl">
                     <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
+                        <h1 className="text-center underline">Order Summery</h1>
+                        <h2>Selected Item: {cart.length}</h2>
                         <p>If a dog chews shoes whose shoes does he choose?</p>
                         <div className="card-actions justify-end">
                         <button className="btn btn-success w-full">Buy Now</button>
