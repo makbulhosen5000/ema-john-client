61b3cb191a7178c39d769c962b7d1b1242156285
mh akash <mhakash5000@gmail.com>
2023-03-28 17:24:40 +0600

    Added product component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,22 +3,34 @@ import React from 'react';
 const Cart = ({cart}) => {
     console.log(cart);
     
-    let total = 0;
+    let totalPrice = 0;
     let totalShipping = 0
+    let quantity = 0
     for(const product of cart){
-        total = total + product.price;
+
+        //condition 1
+        // if(product.quantity === 0){
+        //     product.quantity = 1;
+        // }
+        // condition 1 condition 2 will display save result
+        //condition 2
+        //product.quantity = product.quantity || 1;
+
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
-    const tax = total * 10/100;
-    const totalPrice = total + totalShipping + tax;
+    const tax = totalPrice * 10/100;
+    const grandTotal = totalPrice + totalShipping + tax;
+
     return (
         <div >
              <h1 className="text-center underline">Order Summery</h1>
-             <h2>Selected Item: {cart.length}</h2>
-             <h2>Total Price:$ {total.toFixed(2)}</h2>
-             <h2>Total Shipping:$ {totalShipping.toFixed(2)} </h2>
+             <h2>Selected Item: {quantity}</h2>
+             <h2>Total Price:$ {totalPrice}</h2>
+             <h2>Total Shipping:$ {totalShipping} </h2>
              <h2>Total Tax:$ {tax.toFixed(2)}</h2>
-             <h2>Grand Total:$ {totalPrice.toFixed(2)} </h2>
+             <h2>Grand Total:$ {grandTotal.toFixed(2)} </h2>
            
         </div>
     );
